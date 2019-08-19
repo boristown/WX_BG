6b7b8d9342b6bf4d492cd8a0f6f85741bd70ab4d
boristown <boristown@gmail.com>
2019-08-19 19:46:26 +0800

    迭代11：修复读取预测结果的Bug

--- WX_BG.py
+++ WX_BG.py
@@ -21,14 +21,16 @@ predict_files  = glob.glob(predict_file_pattern)
 for predict_file in predict_files:
     os.remove(predict_file)
 
+time.sleep(3)
 #读取价格并生成输入数据
 symbol_id_list = prices.read_prices()
 
 # 预测并读取结果 
 while True:
-    time.sleep(10)
+    time.sleep(1)
     predict_files  = glob.glob(predict_file_pattern)
     if len(predict_files) == 0:
         continue
+    time.sleep(3)
     prediction.get_prediction(symbol_id_list, predict_files[0])
     break
--- prediction.py
+++ prediction.py
@@ -8,11 +8,26 @@ import datetime
 def get_prediction(symbol_id_list, prediction_file):
     with open(prediction_file,"r") as fcsv:
         csvreader = csv.reader(fcsv,delimiter = ",")
-        predict_batch_size = len(list(csvreader))
         prices_batch_size = len(symbol_id_list)
-        if predict_batch_size != prices_batch_size:
-            print( "预测结果条目为：" + str(predict_batch_size) + "，与输入价格条目数:" + str(prices_batch_size) + "不匹配！" )
+
+        insert_val = []
+        predict_index = 0
+        for row in csvreader:
+            insert_val.append = (
+                                 symbol_id_list[predict_index], 
+                                 datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),
+                                 row[1], row[3], row[5], row[7], row[9], row[11], row[13], row[15], row[17], row[19]
+                                 )
+            predict_index += 1
+
+        if predict_index != prices_batch_size:
+            print( "预测结果条目为：" + str(predict_index) + "，与输入价格条目数:" + str(prices_batch_size) + "不匹配！" )
             return None
+
+        if len(insert_val) == 0:
+            print("读取预测结果失败！")
+            return None
+
         mydb = mysql.connector.connect(
             host=mypsw.wechatadmin.host, 
             user=mypsw.wechatadmin.user, 
@@ -25,19 +40,6 @@ def get_prediction(symbol_id_list, prediction_file):
             ") VALUES (" \
             "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
 
-        insert_val = []
-        row_index = 0
-        for row in csvreader:
-            insert_val.append = (
-                                 symbol_id_list[row_index], 
-                                 datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),
-                                 row[1], row[3], row[5], row[7], row[9], row[11], row[13], row[15], row[17], row[19]
-                                 )
-            row_index += 1
-
-        if len(insert_val) == 0:
-            print("读取预测结果失败！")
-            return None
 
         mycursor.executemany(insert_sql, insert_val)
 
